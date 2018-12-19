d26ccb422c003d6ad42e98d33a3e80ea014bdac0
Lucas Costa <lucasrodcosta@gmail.com>
2018-12-19 11:00:04 -0200

    Bump version

--- kubeexec.rb
+++ kubeexec.rb
@@ -1,8 +1,8 @@
 class Kubeexec < Formula
   desc "Enables you to run commands in multiple k8s pods at once"
   homepage "https://github.com/lucasrodcosta/kubeexec"
-  url "https://github.com/lucasrodcosta/kubeexec/archive/0.0.1.tar.gz"
-  sha256 "d12fca8a05f9bbd7b44643ccbdefbecfebfe82628993c40ed9813efaa17ca940"
+  url "https://github.com/lucasrodcosta/kubeexec/archive/0.0.2.tar.gz"
+  sha256 "4f55ff0852ad77e293525c481e23f6f560954d17c501aead5c115a127078362f"
   head "https://github.com/lucasrodcosta/kubeexec.git"
 
   depends_on "itermocil"
